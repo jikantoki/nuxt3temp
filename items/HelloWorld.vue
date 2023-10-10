bb5175d7e5c2780d1d552973e7663eb2ffb22b89
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-10-10 16:06:23 +0900

    requireを解消

--- items/HelloWorld.vue
+++ items/HelloWorld.vue
@@ -2,12 +2,7 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+        <v-img src="~/assets/logo.svg" class="my-3" contain height="200" />
       </v-col>
 
       <v-col class="mb-4">
@@ -83,49 +78,49 @@ export default {
     ecosystem: [
       {
         text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next'
+        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
       },
       {
         text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next'
+        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
       },
       {
         text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
-      }
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
     ],
     importantLinks: [
       {
         text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
+        href: 'https://community.vuetifyjs.com',
       },
       {
         text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify'
+        href: 'https://madewithvuejs.com/vuetify',
       },
       {
         text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
+        href: 'https://twitter.com/vuetifyjs',
       },
       {
         text: 'Articles',
-        href: 'https://medium.com/vuetify'
-      }
+        href: 'https://medium.com/vuetify',
+      },
     ],
     whatsNext: [
       {
         text: 'Explore components',
-        href: 'https://vuetifyjs.com'
+        href: 'https://vuetifyjs.com',
       },
       {
         text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/'
+        href: 'https://vuetifyjs.com/introduction/roadmap/',
       },
       {
         text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
-    ]
-  })
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
 }
 </script>
