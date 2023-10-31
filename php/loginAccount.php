81237d9d449714f45824bdfc0b54a22e4a40479f
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-10-31 12:21:43 +0900

    fix

--- php/loginAccount.php
+++ php/loginAccount.php
@@ -1,7 +1,7 @@
 <?php
 require_once './settings.php'; //env読み込み用、これを読み込まないと動かないもの多い
 require_once './functions/database.php';
-require_once './authAPI.php';
+require_once './functions/authAPI.php';
 require_once './functions/authAPIforUse.php'; //APIが有効かどうか自動判定
 
 if (
