31968795f513a99fa23d0f076b32d49929cdb7f7
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-11-10 00:22:33 +0900

    add getProfile.php

---
+++ php/getProfile.php
@@ -0,0 +1,37 @@
+<?php
+require_once './settings.php'; //env読み込み用、これを読み込まないと動かないもの多い
+require_once '../vendor/autoload.php';
+require_once '../env.php';
+require_once './functions/database.php';
+require_once './functions/authAPI.php';
+require_once './functions/authAPIforUse.php'; //APIが有効かどうか自動判定
+require_once './functions/mailFunctions.php';
+
+if (
+  !isset($_GET['id'])
+) {
+  echo json_encode([
+    'status' => 'ng',
+    'reason' => 'invalid GET params',
+    'errCode' => 10
+  ]);
+  exit;
+}
+
+$id = $_GET['id'];
+$res = getProfile($id);
+if ($res) {
+  echo json_encode([
+    'status' => 'ok',
+    'reason' => 'Thank you!',
+    'res' => $res,
+    'id' => $id
+  ]);
+} else {
+  echo json_encode([
+    'status' => 'ng',
+    'reason' => 'Unknown user',
+    'id' => $id,
+    'errCode' => 20
+  ]);
+}
